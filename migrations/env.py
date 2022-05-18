82f4fa6b8444a17c8da72a2fb9cad5de1a70a895
Amadeusz Klimaszewski <ami0126538815@gmail.com>
2022-05-18 18:39:32 +0200

    Structure cleanup

--- migrations/env.py
+++ migrations/env.py
@@ -5,14 +5,17 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from decouple import config as decouple_config
+from src.settings.database import DatabaseSettings
+
+from src.apps.users.models import User
+from src.database.connection import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
 
 
-config.set_main_option("sqlalchemy.url", decouple_config("DATABASE_URL"))
+config.set_main_option("sqlalchemy.url", DatabaseSettings.postgres_url)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -23,7 +26,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -69,9 +72,7 @@ def run_migrations_online():
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
