627785d112f9fe2bdea5e9c218becc7d51f3e8c1
Amadeusz Klimaszewski <ami0126538815@gmail.com>
2022-05-20 17:28:29 +0200

    Recipe schemas

---
+++ src/apps/recipes/schemas.py
@@ -0,0 +1,20 @@
+from uuid import UUID
+from pydantic import BaseModel, Field
+
+
+class RecipeInputSchema(BaseModel):
+    title: str = Field(..., max_length=30)
+    time_required: str
+    servings: int
+    description: str
+
+
+class RecipeOutputSchema(BaseModel):
+    id: UUID
+    user_id: UUID
+    title: str = Field(..., max_length=30)
+    time_required: str
+    servings: int
+    description: str
+    review_average: float
+    review_count: int
